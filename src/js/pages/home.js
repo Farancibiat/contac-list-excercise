18449dbb5cd9bea55dce9e20df9f54e60245d476
Pehuen <arancibiat.felipe@gmail.com>
2021-05-04 21:30:30 -0400

    contactCard advanced, fetch to  read contacts it's completed

--- src/js/pages/home.js
+++ src/js/pages/home.js
@@ -1,17 +1,36 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Button } from "react-bootstrap";
+import { ContactCard } from "../component/contactCard";
+import { Context } from "../store/appContext";
+
 //create your first component
+
 export function Home() {
+	const { store, actions } = useContext(Context);
 	return (
-		<>
-			<div className="container py-2">
-				<div className="row">
-					<div className="col-sm-10"></div>
-					<div className="col-12 col-sm-2">
-						<Button variant="success">Add new contact</Button>
-					</div>
+		<div className="container">
+			<div className="row py-3">
+				<div className="col-sm-10"></div>
+				<div className="col-12 col-sm-2 text-end px-0">
+					<Button variant="success">Add new contact</Button>
 				</div>
 			</div>
-		</>
+			{store.contacts.length < 1 ? (
+				<div className="bg-primary text-success">Loading...</div>
+			) : (
+				store.contacts.map((info, index) => {
+					return (
+						<div key={`${index}`} className="row">
+							<ContactCard
+								name={`${info.full_name}`}
+								email={`${info.email}`}
+								dir={`${info.address}`}
+								phone={`${info.phone}`}
+							/>
+						</div>
+					);
+				})
+			)}
+		</div>
 	);
 }
--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,7 +1,21 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
-		store: {},
-		actions: {}
+		store: {
+			contacts: []
+		},
+		actions: {
+			getContacts: () => {
+				fetch(
+					"https://assets.breatheco.de/apis/fake/contact/agenda/pehuens"
+				)
+					.then(resp => resp.json())
+					.then(data => {
+						setStore({ contacts: data });
+						console.log(data);
+					})
+					.catch(err => setStore({ contacts: err }));
+			}
+		}
 	};
 };
 export default getState;
